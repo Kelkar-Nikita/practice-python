46b3a3283e961ce81027d28b7e155072e8cf45c4
karanbopche <karanbopche@gmail.com>
2021-01-30 16:38:54 +0530

    new commit

--- comp.py
+++ comp.py
@@ -1,14 +1,16 @@
-name=["alice","bob"]
-title=["software","hardware"]
-dict1={i:j for i,j in zip(name,title)}
+name = ["alice", "bob", "radha"]
+title = ["Data software", "hardware", "Data Scientist"]
+dataPersons = {n: t for n, t in zip(name, title) if "Data" in t}
+print(dataPersons)
+dict1 = {i: j for i, j in zip(name, title)}
 for key in dict1:
     print(key)
 print()
 for value in dict1.values():
     print(value)
 print()
-for key,value in zip(dict1.keys(), dict1.values()):
+for key, value in zip(dict1.keys(), dict1.values()):
     print(key, value)
 print()
 for key, value in dict1.items():
-  print(key, value)
+    print(key, value)
