0205fe2d19392960644b5b469fd2d2d3310150aa
Kelkar-Nikita <77345053+Kelkar-Nikita@users.noreply.github.com>
2021-01-30 15:11:47 +0530

    add implementation for "for loops"
    used values and items function on dictionary.

--- comp.py
+++ comp.py
@@ -1,4 +1,14 @@
 name=["alice","bob"]
 title=["software","hardware"]
-dict={i:j for i,j in zip(name,title)}
-print(dict)
+dict1={i:j for i,j in zip(name,title)}
+for key in dict1:
+    print(key)
+print()
+for value in dict1.values():
+    print(value)
+print()
+for key,value in zip(dict1.keys(), dict1.values()):
+    print(key, value)
+print()
+for key, value in dict1.items():
+  print(key, value)
